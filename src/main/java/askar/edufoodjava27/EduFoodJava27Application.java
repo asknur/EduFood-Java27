a493475a27b7d8933d5d9fe2cd27e8107e7515c5
Askar <nurbekovaskar099@gmail.com>
2026-04-29 18:51:51 +0600

    Starting pack

---
+++ src/main/java/askar/edufoodjava27/EduFoodJava27Application.java
@@ -0,0 +1,13 @@
+package askar.edufoodjava27;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduFoodJava27Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduFoodJava27Application.class, args);
+    }
+
+}
