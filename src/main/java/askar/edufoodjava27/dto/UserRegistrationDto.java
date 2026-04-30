d5c4c573c0cebc60a0c9b202dbbe2b35d48ee721
Askar <nurbekovaskar099@gmail.com>
2026-04-30 18:54:53 +0600

    created user reg dto

---
+++ src/main/java/askar/edufoodjava27/dto/UserRegistrationDto.java
@@ -0,0 +1,25 @@
+package askar.edufoodjava27.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UserRegistrationDto {
+    @NotBlank(message = "Имя не может быть пустым")
+    private String name;
+
+    @NotBlank(message = "Email не может быть пустым")
+    @Email(message = "Некорректный формат email")
+    private String email;
+
+    @NotBlank(message = "Пароль не может быть пустым")
+    @Size(min = 4, message = "Пароль должен содержать минимум 4 символа")
+    private String password;
+}
